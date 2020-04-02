783861b0ae7b731070be82bf1f6469a142c9a956
jinp-jiang <63032366+jinp-jiang@users.noreply.github.com>
2020-04-02 14:00:17 +0800

    bspAll.sh
    Collect script integration

---
+++ bspAll.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#for date in `cat /home/jcdcn/bspLogHandle/dateList`;
+#       do
+        /usr/bin/sh /home/STD-MO/bspLog/bspCollect.sh DP
+#done
+sleep 3
+#for date in `cat /home/jcdcn/bspLogHandle/dateList`;
+#        do
+        /usr/bin/sh /home/STD-MO/bspLog/bspCollect.sh LED
+#done
+sleep 3
+#for date in `cat /home/jcdcn/bspLogHandle/dateList`;
+#        do
+        /usr/bin/sh /home/STD-MO/bspLog/bspCollect.sh SPE
+#done
