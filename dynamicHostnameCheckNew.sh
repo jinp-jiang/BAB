ce5c3dc8b65d2303a1f63016f131f0d6a9a3e34f
jinp-jiang <63032366+jinp-jiang@users.noreply.github.com>
2020-05-28 13:40:15 +0800

    Update dynamicHostnameCheckNew.sh

--- dynamicHostnameCheckNew.sh
+++ dynamicHostnameCheckNew.sh
@@ -5,20 +5,32 @@ ansibleHostsPath="/home/check/ansibleHosts"
 ansibleFinalFile="/home/STD-MO/bspLog/proof/ansibleFinalHosts"
 logFile="/home/STD-MO/bspLog/proof/log/checkResultNew.log"
 cat /dev/null > ${ansibleFinalFile}
-#Save the original hosts file before processing
+#处理前保存原来的hosts文件
 cp "$originalHosts" "$originalHosts""$lsDate"
 cat /dev/null > $logFile
-
-for hostname in `ls ${ansibleHostsPath}`;
+IFS=$'\n'
+for hostname in `cat /home/STD-MO/bspLog/proof/hostNameList`;
 do
-	cat ${ansibleHostsPath}/${hostname} >> ${ansibleFinalFile}		
+        echo ${hostname}
+        if [ -s ${ansibleHostsPath}/${hostname} ];then
+                echo "normal" > /dev/null
+        else
+                su STD-MO -c "ansible ${hostname}.jcd.priv -b --become-user root --become-method sudo -m shell -a '/usr/bin/sh /home/STD-MO/recordInfoNew.sh'"
+        fi
+        cat ${ansibleHostsPath}/${hostname} >> ${ansibleFinalFile}
 done
 IFS=$'\n'
 for player in `cat ${ansibleFinalFile}`;
 do
-	v="/home/STD-MO/bspLog/proof/v"
-	echo "${player}" > ${v}
-	a=`awk '{print $1}' ${v}`
-	tmp=`grep "${a}" ${originalHosts}`
-	sed -i "s/${tmp}/${player}/g" /etc/ansible/hosts
+        v="/home/STD-MO/bspLog/proof/v"
+        echo "${player}" > ${v}
+        a=`awk '{print $1}' ${v}`
+        b=`awk '{print $2}' ${v}`
+        tmp=`grep "${a}" ${originalHosts}`
+        tmpIP=`grep "${a}" ${originalHosts} | awk '{print $2}'`
+        if [ "${tmpIP}" != "${b}" ];then
+                sed -i "s/${tmp}/${player}/g" /etc/ansible/hosts
+        else
+		echo "Normal"
+        fi
 done
